d6d9fedea5199ba09968b7e287b125d807b35e38
ashirwadexe <ashirwadbappy@gmail.com>
2024-11-09 13:55:09 +0530

    update profile

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -53,17 +53,17 @@ function Navbar() {
                         <Popover>
                             <PopoverTrigger>
                                 <Avatar className='cursor-pointer'>
-                                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                    <AvatarImage src={user?.profile?.profilePhoto} alt="@shadcn" />
                                 </Avatar>
                             </PopoverTrigger>
                             <PopoverContent className='w-80'>
                                 <div className='flex gap-4 space-y-2'>
                                     <Avatar className='cursor-pointer'>
-                                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                        <AvatarImage src={user?.profile?.profilePhoto} alt="@shadcn" />
                                     </Avatar>
                                     <div>
-                                        <h4 className='font-medium'>Ashirwad Chaurasia</h4>
-                                        <p className='text-sm text-muted-foreground'>This is about me.</p>
+                                        <h4 className='font-medium'>{user?.fullname}</h4>
+                                        <p className='text-sm text-muted-foreground'>{user?.profile?.bio}</p>
                                     </div>
                                 </div>
                                 <div className='flex flex-col text-gray-600'>
