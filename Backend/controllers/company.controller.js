9c7628500cdb1fa1d91191ee50f3d382945e4c40
ashirwadexe <ashirwadbappy@gmail.com>
2024-11-10 18:38:23 +0530

    cloudinary for company logo

--- Backend/controllers/company.controller.js
+++ Backend/controllers/company.controller.js
@@ -1,4 +1,6 @@
 import { Company } from "../models/company.model.js";
+import getDataUri from '../utils/datauri.js';
+import cloudinary from '../utils/cloudinary.js';
 
 export const registerCompany = async (req, res) => {
     try {
@@ -26,7 +28,7 @@ export const registerCompany = async (req, res) => {
         });
 
         return res.status(200).json({
-            message:"Company created successfully",
+            message:"Company Created successfully",
             success: true,
             company
         });
@@ -83,8 +85,11 @@ export const updateCompany = async (req, res) => {
         const { name, description, website, location } = req.body;
         const file = req.file;
         // idhar cloudinary aaega
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
 
-        const updateData = { name, description, website, location };
+        const updateData = { name, description, website, location, logo };
 
         const company = await Company.findByIdAndUpdate(req.params.id, updateData, {new: true});
 
@@ -96,7 +101,7 @@ export const updateCompany = async (req, res) => {
         }
 
         return res.status(200).json({
-            message: "company information updated successfully",
+            message: "Company Updated Successful",
             success: true
         });
     } catch (error) {
